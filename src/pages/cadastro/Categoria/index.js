421f3e1ec26fbe6b0bc5c6f9ea11c0369efa2578
Carlos Henrique <carloshenrique@casamagalhaes.com.br>
2020-07-29 16:47:58 -0300

    Criando formulários

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,25 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 
 import { Link } from 'react-router-dom';
 
 export default function CadastroCategoria() {
+  const [ categorias, setCategorias ] = useState(['Teste', 'Categoria2']);
+  const [ values, setValues ] = useState({
+    nome: '',
+    descricao: '',
+    cor: '#000000',
+  });
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCategorias([...categorias, values]);
+  }
+
   return (
     <PageDefault>
-      <h1>Página de Cadastro de Categoria</h1>
+      <h1>Página de Cadastro de Categoria: { values.nome }</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
+
+      <div>
+
         <label>
-          Nome da Categoria:
-          <input
+            Nome da Categoria:
+            <input
+              type="text"
+              value={values.nome}
+              onChange={(e) => {
+                setValue('nome', e.target.value);
+              }}
+            />
+        </label>
+
+      </div>
+
+      <div>
+        <label>
+          Descrição:
+          <textarea
             type="text"
+            value={values.descricao}
+            onChange={(e) => {
+              setValue('descricao', e.target.value);
+            }}
           />
         </label>
+      </div>
+
+      <div>
+        <label>
+          Cor:
+          <input
+            type="color"
+            value={values.cor}
+            onChange={(e) => {
+              setValue('cor', e.target.value);
+            }}
+          />
+        </label>
+      </div>
 
         <button>
           Cadastrar
         </button>
       </form>
+
+      
 
       <Link to="/">
         Ir para home
