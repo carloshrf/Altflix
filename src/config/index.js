0361339ffa22293fb4d279a56d4b70523acd43fe
carloshrf <carlos_6282010@hotmail.com>
2020-08-03 00:14:55 -0300

    corrigindo url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://altflix.herokuapp.com/';
+  : 'https://altflix.herokuapp.com';
 
 export default URL_BACKEND_TOP;
