cda07daf3b3741908c6d99744cf929af3952e16e
carloshrf <carlos_6282010@hotmail.com>
2020-08-03 23:40:31 -0300

    Corrigido erro ao acessar a home sem nenhum vídeo cadastrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import PageDefault from '../../components/PageDefault';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 
 import categoriasRepository from '../../repositories/categorias';
+
+const Warning = styled.h2`
+  text-align: center;
+  padding-top: 30px;
+`;
 
 function Home() {
   const [dadosIniciais, setDadosIniciais] = useState([]);
@@ -11,6 +17,7 @@ function Home() {
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
       .then((categoriasComVideos) => {
+        console.log('categorias', categoriasComVideos);
         setDadosIniciais(categoriasComVideos);
       })
       .catch((err) => {
@@ -23,30 +30,32 @@ function Home() {
 
       {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-      {dadosIniciais.map((categoria, indice) => {
-        if (indice === 0) {
-          return (
-            <div key={categoria.id}>
-              <BannerMain
-                videoTitle={dadosIniciais[0].videos[0].titulo}
-                url={dadosIniciais[0].videos[0].url}
-                videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e Javascript"
-              />
-              <Carousel
-                ignoreFirstVideo
-                category={dadosIniciais[0]}
-              />
-            </div>
-          );
-        }
+      {dadosIniciais.length >= 1
+        ? dadosIniciais.map((categoria, indice) => {
+          if (indice === 0) {
+            return (
+              <div key={categoria.id}>
+                <BannerMain
+                  videoTitle={dadosIniciais[0].videos[0].titulo}
+                  url={dadosIniciais[0].videos[0].url}
+                  videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e Javascript"
+                />
+                <Carousel
+                  ignoreFirstVideo
+                  category={dadosIniciais[0]}
+                />
+              </div>
+            );
+          }
 
-        return (
-          <Carousel
-            key={categoria.id}
-            category={categoria}
-          />
-        );
-      })}
+          return (
+            <Carousel
+              key={categoria.id}
+              category={categoria}
+            />
+          );
+        })
+        : <Warning>Não há vídeos disponíveis :(. Adicione clicando em Novo vídeo!</Warning>}
 
     </PageDefault>
   );
